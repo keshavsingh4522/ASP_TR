535e95d8fb583a3560f005b0b7610c6d57bcf735
Keshav Singh <keshavsingh4522@gmail.com>
2021-03-05 01:47:05 -0800

    simple application

---
+++ simple_01/Controllers/HelloWrldController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace simple_01.Controllers
+{
+    public class HelloWrldController : Controller
+    {
+        /*        public IActionResult Index()
+                {
+                    return View();
+                }
+        */
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome1(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
