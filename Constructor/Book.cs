bb9a09b2172dfea94896f1ce1636c8c855e44c14
Keshav Singh <keshavsingh4522@gmail.com>
2021-03-10 15:45:04 +0530

    Constructor in c#

---
+++ Constructor/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructor
+{
+    class Book
+    {
+        public string author;
+        public string title;
+        public int pages;
+        
+        //constructor
+        public Book(string book_name,string book_author,int book_page)
+        {
+            title = book_name;
+            author = book_author;
+            pages = book_page;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Book title : {title}");
+            Console.WriteLine($"Book author : {author}");
+            Console.WriteLine($"Book page : {pages}\n");
+        }
+    }
+}
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("I Love You","Keshav Singh",123);
+            Book book2 = new Book("PK","PR",321);
+
+            book1.Display();
+            book2.Display();
+        }
+    }
+}
