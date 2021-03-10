410a6953529e8c8892a5bd3018df9a4db3059e3d
Keshav Singh <keshavsingh4522@gmail.com>
2021-03-10 12:08:45 +0530

    Class and Object

---
+++ ConsoleProgramming/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleProgramming
+{
+    class Book
+    {
+        public string author;
+        public string title;
+        public int pages;
+
+        public void Display()
+        {
+            Console.WriteLine($"Book title : {title}");
+            Console.WriteLine($"Book author : {author}");
+            Console.WriteLine($"Book page : {pages}\n");
+        }
+    }
+}
---
+++ ConsoleProgramming/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.title = "I Love You";
+            book1.author = "Keshav Singh";
+            book1.pages = 123;
+
+            Book book2 = new Book();
+            book2.title = "PK";
+            book2.author = "Pr";
+            book2.pages = 321;
+
+            book1.Display();
+            book2.Display();
+        }
+    }
+}
