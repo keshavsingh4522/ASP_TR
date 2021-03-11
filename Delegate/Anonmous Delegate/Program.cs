5d5b7019a8aeaacd4f6afb46fa60b4b1cbe97684
Keshav Singh <keshavsingh4522@gmail.com>
2021-03-11 14:52:51 +0530

    Anonymous delegate

---
+++ Delegate/Anonmous Delegate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Anonmous_Delegate
+{
+    class Program
+    {
+        public delegate int MyDelegate(int arg1, int arg2);
+        static void Main(string[] args)
+        {
+            MyDelegate f = delegate (int a1, int a2)
+            {
+                return a1 * a2;
+            };
+            Console.WriteLine("The Number is {0}",f(20,19));
+        }
+    }
+}
