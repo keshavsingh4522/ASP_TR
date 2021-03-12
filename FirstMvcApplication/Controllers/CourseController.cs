2fcea46d252d75d678b732428c2cf17befe34e6a
Keshav Singh <keshavsingh4522@gmail.com>
2021-03-12 16:07:34 +0530

    first mvc application

---
+++ FirstMvcApplication/Controllers/CourseController.cs
@@ -0,0 +1,106 @@
+﻿using FirstMvcApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace FirstMvcApplication.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Course/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Course/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Course/Create
+        [HttpPost]
+        public ActionResult Create(Course course)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    // TODO: Add insert logic here
+
+                    return RedirectToAction("Index");
+                }
+                return View(course);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Course/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Course/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Course/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Course/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult Review()
+        {
+            return View();
+        }
+        public ActionResult Review(CourseReview review)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ FirstMvcApplication/readme.md
@@ -0,0 +1,6 @@
+- STEPS
+```
+1. Add a class by clicking on model folder --> add --> class	
+2. Add property there
+  type prop --> hit tab twice
+```
