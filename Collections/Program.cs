a99c6dd1f2e14b7ca0ccb171faf646fb04c806ec
Keshav Singh <keshavsingh4522@gmail.com>
2021-03-13 09:06:34 +0530

    ArrayList readme updated

---
+++ Collections/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.
+            ArrayList arr = new ArrayList();
+            Console.WriteLine(arr.Capacity);
+            arr.Add(200);
+            Console.WriteLine(arr.Capacity);
+            arr.Add(123); arr.Add(3); arr.Add(34);
+            Console.WriteLine(arr.Capacity);
+            arr.Add(123);
+            Console.WriteLine(arr.Capacity);
+            Console.WriteLine("----------------------------------------");
+
+            // 2
+            ArrayList ar = new ArrayList(2);
+            Console.WriteLine(ar.Capacity);
+            ar.Add(200);
+            Console.WriteLine(ar.Capacity);
+            ar.Add(123); ar.Add(3); ar.Add(34);
+            Console.WriteLine(ar.Capacity);
+            ar.Add(123);
+            Console.WriteLine(ar.Capacity);
+
+            // 3
+            foreach (object obj in ar)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
+
+
+            // remove by value
+            // ar.Remove(1230); this will not give error when value is not present
+            ar.Remove(123);
+            foreach (object obj in ar)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
+            // remove by index
+            // it will give error if index is not present or out of range/bound
+            ar.RemoveAt(1);
+            foreach (object obj in ar)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
--- Collections/Readme.md
+++ Collections/Readme.md
@@ -1,37 +1,6 @@
 ﻿# Collections
-- Array
-```C#
-synatx:
- int[] arr = new int[10];
 
-// these below 3 things are not possible with array
-- increasing the size
-- inserting values into the middle
-- Deleting or removing value
-
-// these above are poosible with ArrayList in Collection
-
-````
 - Non Generic Collections
 ```
 Stack, Queue, LinkedList,SortedList,ArrayList,hashtable
-```
-- ArrayList
-```C#
-ArrayList al = new ArrayList();
-al.add(100);
-Console.WriteLine(al.Capacity) // 4
-// after adding 5 item , the capacity will double  as become 8
-// after adding 9th item the capacity will also double to 16 ...
-```
-```C#
-// we can assign the size to ArrayList in start as
-ArrayList al = new ArrayList(10);
-
-// after adding 11th item its size will double as 20 and so on ...
-```
-```
-al.insert(index,value);
-al.Remove(value);
-al.RemoveAt(index);
 ```
